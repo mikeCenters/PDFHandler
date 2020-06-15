e15e83c080687491ecba5a11f609669d2d640d27
Mike Centers <michael.centers@icloud.com>
2020-06-15 10:37:17 -0400

    Initial Commit

---
+++ PDFHandler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDFHandler
+//
+//  Created by Mike Centers on 6/15/20.
+//  Copyright © 2020 Mike Centers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
